4149dfbb76171c3c39acb3cbf16f86f6e684cde7
Bingzhi.Li <280761575@qq.com>
2020-11-20 23:07:00 +0800

    Add avatar source code

---
+++ client/QClient/QAvatar.cpp
@@ -0,0 +1,27 @@
+﻿#include "QAvatar.h"
+#include"qpixmap.h"
+#include <QBitmap>
+
+QAvatar::QAvatar(QPixmap *avatar, QSize size, EN_AVATAR_SHAPE shape, int x, int y, bool isclickable, QWidget *parent):
+	m_AvaFile(*avatar),
+	m_AvaSize(size),
+	m_AvaShape(shape),
+	m_AvaIsClickable(isclickable),
+	m_AvaPosX(x),
+	m_AvaPosY(y),
+	QPushButton(parent)
+{
+	QPixmap pixmap("Resources/HeadPortraits.png");
+	setMask(QBitmap(pixmap.mask()));
+	
+	this->setGeometry(m_AvaPosX,m_AvaPosY,m_AvaSize.rwidth(),m_AvaSize.rheight());
+	//this->setEnabled(isEnabled);
+
+	setStyleSheet(QString("QPushButton{background-image:url(Resources/QQImage.png);border:none}"));
+}
+
+
+QAvatar::~QAvatar()
+{
+
+}
---
+++ client/QClient/QAvatar.h
@@ -0,0 +1,35 @@
+﻿#ifndef _QAVATAR_H_
+#define _QAVATAR_H_
+
+#include <QPushButton>
+
+typedef enum
+{
+	E_AVATAR_SHAPE_CIRCLE,
+	E_AVATAR_SHAPE_SQUARE
+	
+}EN_AVATAR_SHAPE;
+
+class QAvatar : public QPushButton
+{
+	Q_OBJECT
+
+public:
+	QAvatar::QAvatar(QPixmap* avatar, QSize size, EN_AVATAR_SHAPE shape, int x, int y, bool isclickable, QWidget* parent); 
+	QAvatar(QWidget *parent);
+	~QAvatar();
+
+	
+private:
+    //avatar nature
+    QPixmap         m_AvaFile;
+    QSize           m_AvaSize;
+    EN_AVATAR_SHAPE m_AvaShape;
+    bool            m_AvaIsClickable;
+    int             m_AvaPosX;
+    int             m_AvaPosY;
+	
+	
+};
+
+#endif //_QAVATAR_H_
